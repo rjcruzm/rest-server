a202244c2ba63990e12419dba4c6ad38eae5f2e9
raul cruz <raul_cruz@trimble.com>
2022-11-04 14:52:00 -0700

    Google signin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const bcryptjs = require('bcryptjs');
 const { response } = require('express');
+const { json } = require('express/lib/response');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/usuario')
 
 const login = async (req, res = response) => {
@@ -46,6 +48,51 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            const data = {
+                nombre,
+                correo,
+                password: 'wer',
+                img,
+                rol: 'USER',
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg: 'El token no se pudo verificar - 1'
+        })
+    }
+
+    
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
